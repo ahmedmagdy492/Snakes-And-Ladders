12e3fcfddb4670764b7d82e21c64fbc58d16dce8
ahmed magdy <ahmedmagdy492@yahoo.com>
2025-03-17 05:44:52 +0200

    removed unnecessary files

--- UI/IClickable1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SnakeAndLadders.UI
-{
-    public interface IClickable1
-    {
-        event Action<UIElement, UIEvent> OnClick;
-    }
-}
--- UI/IClickable2.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SnakeAndLadders.UI
-{
-    public interface IClickable2
-    {
-        event Action<UIElement, UIEvent> OnClick;
-    }
-}
